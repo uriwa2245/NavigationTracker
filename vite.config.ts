6aeaeacf809030bc896d6a658b37086f4f8e63c6
uriwa2245 <96137384+uriwa2245@users.noreply.github.com>
2025-07-29 10:09:27 +0700

    Fix WebSocket HMR connection errors

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,10 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
+    hmr: {
+      clientPort: 443,
+      host: true,
+    },
     fs: {
       strict: true,
       deny: ["**/.*"],
